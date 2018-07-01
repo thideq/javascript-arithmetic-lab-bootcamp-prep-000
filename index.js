36e90b9a9d8cdfd6d2336bbe7fd0478e3bee0199
Thierry Dequin <thierrydequin@hotmail.com>
2018-07-01 13:48:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-var a, b
+var a, b, n
     number = 5
 
 function add(a, b) {
   return a + b
 }
 
-function substract(a, b)
+function substracts(a, b)
  {
   return a - b
 }
 
-function numberinc(number) {
-  return number + 1
+function multiply(a, b)
+ {
+  return a * b
 }
-function numberdec(number) {
-  return number - 1
+function divide(a, b)
+ {
+  return a / b
 }
-
-
+function inc(n) {
+  return n + 1
+}
+function dec(n) {
+  return n - 1
+}
+function makeInt(n) {
+  return parseInt(n,10)
+}
+function makeInt(n) {
+  return parseInt(n)
+}
 add(number, 3)
 console.log(add(number, 3))
 
